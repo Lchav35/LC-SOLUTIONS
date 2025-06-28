c50a783838a2cf2b83d630fcd1c09566cc725d64
Lchav35 <82476949+Lchav35@users.noreply.github.com>
2025-06-28 15:17:56 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "seu-projeto.firebaseapp.com",
+  projectId: "seu-projeto",
+  storageBucket: "seu-projeto.appspot.com",
+  messagingSenderId: "ID",
+  appId: "APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.auth = auth;
